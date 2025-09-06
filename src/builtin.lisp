6cdc547341819f7b408deae2737475b9af027c62
Lukáš Hozda <luk.hozda@gmail.com>
2025-09-06 12:57:12 +0200

    Add shoelace elements

--- src/builtin.lisp
+++ src/builtin.lisp
@@ -21,4 +21,15 @@
   q rp rt ruby s samp script section select small source span strong
   style sub summary sup svg table tbody td template textarea tfoot th
   thead |time| title tr track u ul var video wbr
-  <> raw!)
+  <> raw!
+  ;; shoelace tags
+  sl-alert sl-animated-image sl-animation sl-avatar sl-badge sl-breadcrumb
+  sl-breadcrumb-item sl-button sl-button-group sl-card sl-carousel sl-carousel-item
+  sl-checkbox sl-color-picker sl-copy-button sl-details sl-dialog sl-divider
+  sl-drawer sl-dropdown sl-format-bytes sl-format-date sl-format-number
+  sl-icon sl-icon-button sl-image-comparer sl-include sl-input sl-menu
+  sl-menu-item sl-menu-label sl-mutation-observer sl-option sl-popup
+  sl-progress-bar sl-progress-ring sl-qr-code sl-radio sl-radio-button
+  sl-radio-group sl-range sl-rating sl-relative-time sl-resize-observer
+  sl-select sl-skeleton sl-spinner sl-split-panel sl-switch sl-tab sl-tab-group
+  sl-tab-panel sl-tag sl-textarea sl-tooltip sl-tree sl-tree-item sl-visually-hidden)
